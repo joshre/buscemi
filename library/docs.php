9dc1cd5e25a5f918aabfe45e6c6b7492ba9b59bf
joshre <joshre@me.com>
2018-07-19 11:21:30 -0700

    Docfiles

---
+++ library/docs.php
@@ -0,0 +1,16 @@
+<style type="text/css" media="screen">li{ padding-left: 20px;}.docitem{margin-bottom: 10px;}</style>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#wordpress-overview">WordPress Overview</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#logging-in">Logging In</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#media-library">Media Library</a></div>
+<ul>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-a-media-item">Adding a Media Item</a></li>
+</ul>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-editing-a-page">Adding/Editing a Page</a></div>
+<ul>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-a-new-page">Adding A New Page</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#content">Content</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#published-draft-schedulded-private">Published / Draft / Scheduled / Private</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#editing-a-page">Editing a Page</a></li>
+</ul>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-featured-images">Adding a Featured Image</a></div>
+</ul>
